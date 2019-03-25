19a6da15452144307634d4d44e0ce249bdba63da
FastSpace <mikl.zhukov@gmail.com>
2019-03-25 13:47:06 +0300

    Initial commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Михаил on 25/03/2019.
+//  Copyright © 2019 Михаил. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
